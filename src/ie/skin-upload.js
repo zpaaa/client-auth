da2fd9e034a9966b7509aaa20ec7a2db33184bf2
zhupa <zhupa@2345.com>
2019-12-24 19:27:16 +0800

    上传文件页面

--- src/ie/skin-upload.js
+++ src/ie/skin-upload.js
@@ -1,7 +1,82 @@
 import './upload.scss'
 import { headSwitch, getLoginState } from './common'
+import { Validator } from '../utils'
 $(function() {
   headSwitch()
   getLoginState()
   initEvent()
+  initChangeEvent()
+  var fileList = []
+  function uploadVali() {
+    var conpanyValidator = new Validator()
+    var data = getData()
+    conpanyValidator
+      .add('displayName', data.displayName, [
+        { verify: 'isNonEmpty', errMsg: '名称不能为空' },
+        { verify: 'maxLength:20', errMsg: '长度不能超过20个汉字或字符' }
+      ])
+      .add('description', data.description, [
+        { verify: 'isNonEmpty', errMsg: '描述不能为空' },
+        { verify: 'maxLength:10', errMsg: '长度不能超过20个汉字或字符' },
+      ])
+      .add('uploadFile', data.file, [
+        { verify: 'minLength:1', errMsg: '请上传文件' }
+      ])
+    return conpanyValidator
+  }
+
+  function getData () {
+    var data = {}
+    data.displayName = $('#display-name input').val()
+    data.description = $('#description input').val()
+    data.file = fileList
+    return data
+  }
+  function initEvent () {
+    $('#upload-file [type="button"]').on('click', function () {
+      console.log('上传身份证信息')
+      $('#upload-file [type="file"]').click()
+    })
+
+    /* 点击图片的删除 */
+    $('#idCardFrontUploadList').on('click', 'i', function () {
+      var index = $(this).attr('data-index')
+      fileList.splice(index, 1)
+      renderFileList(fileList, 'upload-file')
+    })
+  }
+
+  function initChangeEvent () {
+    $('#upload-file [type="file"]').on('change', function () {
+      fileList = fileList.concat(Array.prototype.slice.call($(this)[0].files))
+      renderFileList()
+    })
+
+    $('#display-name input').on('change', function () {
+      var msg = uploadVali().keyValidator('displayName')
+      if (msg) {
+        $(this).parents('.form-item').addClass('is-error')
+        $(this).parents('.form-item').find('.error-msg').html(msg)
+      }
+    })
+    $('#description input').on('change', function () {
+      var msg = uploadVali().keyValidator('description')
+      if (msg) {
+        $(this).parents('.form-item').addClass('is-error')
+        $(this).parents('.form-item').find('.error-msg').html(msg)
+      }
+    })
+  }
+
+  function renderFileList (data, id) {
+    var str = ''
+    $.each(data, function (index, item) {
+      str += `<span class="file-box" >
+                <span class="file-name" title="${item.name}" >${item.name.split('.')[0]}</span><b>.${item.name.split('.')[1]}</b>
+                <i data-index="${index}"></i>
+              </span>`
+    })
+    // console.log(`#${id}`,str)
+    $(`#${id}`).html(str)
+  }
 })
--- src/utils.js
+++ src/utils.js
@@ -24,3 +24,73 @@ export function getMyIdentity () {
     })
   })
 }
+
+export var vrertifyRules = {
+  isNonEmpty: function (value, errorMsg) {
+    return value === '' ?
+      errorMsg : void 0
+  },
+  minLength: function (value, length, errorMsg) {
+    return value.length < length ?
+      errorMsg : void 0
+  },
+  maxLength: function (value, length, errorMsg) {
+    return value.length > length ?
+      errorMsg : void 0
+  },
+  rightLength: function (value, length, errorMsg) {
+    return value.length = length ?
+      errorMsg : void 0
+  },
+  isMoblie: function (value, errorMsg) {
+    return !/^1(3|5|7|8|9)[0-9]{9}$/.test(value) ?
+      errorMsg : void 0
+  },
+  isEmail: function (value, errorMsg) {
+    return !/^\w+([+-.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value) ?
+      errorMsg : void 0
+  }
+}
+
+export class Validator {
+  constructor() {
+    this.cache = {}
+  }
+  add(key, value, rules) {
+    this.cache[key] || (this.cache[key] = [])
+    for (let rule of rules) {
+      let verifyArr = rule.verify.split(':')
+      let errMsg = rule.errMsg
+      this.cache[key].push(() => {
+        let verify = verifyArr.shift()
+        verifyArr.unshift(value)
+        verifyArr.push(errMsg)
+        return vrertifyRules[verify].apply(key, verifyArr)
+      })
+    }
+    return this
+  }
+  keyValidator(key) {
+    var verifyFnArr = this.cache[key] || []
+    for (var i = 0; i < verifyFnArr.length; i++) {
+      let err = verifyFnArr[i]()
+      if (err) {
+        return err
+      }
+    }
+  }
+  start() {
+    var errMsg = {}
+    for (let key in this.cache) {
+      var verifyFnArr = this.cache[key]
+      for (var i = 0; i < verifyFnArr.length; i++) {
+        let err = verifyFnArr[i]()
+        if (err) {
+          errMsg[key] = err
+          break;
+        }
+      }
+    }
+    return errMsg
+  }
+}
